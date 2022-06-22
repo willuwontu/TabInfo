9ca67f36307566dfa700823657727ca4b3ba64dd
willuwontu <willuwontu3@gmail.com>
2022-06-22 10:56:44 -0700

    Remade the colortester object

---
+++ TabInfo/Monobehaviours/ButtonInteraction.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace TabInfo.Monobehaviours
+{
+    public class ButtonInteraction : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+    {
+        public UnityEvent mouseClick = new UnityEvent();
+        public UnityEvent mouseEnter = new UnityEvent();
+        public UnityEvent mouseExit = new UnityEvent();
+        public Button button;
+        public AudioSource source;
+
+        private System.Random random = new System.Random();
+
+        private void Start()
+        {
+            button = gameObject.GetComponent<Button>();
+            source = gameObject.GetOrAddComponent<AudioSource>();
+
+            mouseEnter.AddListener(OnEnter);
+            mouseExit.AddListener(OnExit);
+            mouseClick.AddListener(OnClick);
+        }
+
+        public void OnEnter()
+        {
+            if (button.interactable)
+            {
+                source.PlayOneShot(TabInfo.instance.hover[random.Next(TabInfo.instance.hover.Count)]);
+            }
+        }
+
+        public void OnExit()
+        {
+            if (button.interactable)
+            {
+                source.PlayOneShot(TabInfo.instance.hover[random.Next(TabInfo.instance.hover.Count)]);
+            }
+        }
+
+        public void OnClick()
+        {
+            if (button.interactable)
+            {
+                source.PlayOneShot(TabInfo.instance.click[random.Next(TabInfo.instance.click.Count)]);
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (button.interactable)
+            {
+                mouseEnter?.Invoke();
+            }
+        }
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (button.interactable)
+            {
+                mouseExit?.Invoke();
+            }
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (button.interactable)
+            {
+                mouseClick?.Invoke();
+            }
+        }
+    }
+}
---
+++ TabInfo/Utils/PlayerCardButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace TabInfo.Utils
+{
+    public class PlayerCardButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public PlayerCardBar cardBar;
+        public CardInfo card;
+
+        private void Start()
+        {
+            this.cardBar = this.gameObject.GetComponentInParent<PlayerCardBar>();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+
+        }
+    }
+}
